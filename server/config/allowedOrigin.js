4e5f2810615aa749e134399e7eba09d7e7cf21c5
codeironside <fury25423@gmail.com>
2024-10-17 10:11:39 +0100

    fixed the login end point

--- server/config/allowedOrigin.js
+++ server/config/allowedOrigin.js
@@ -1,7 +1,8 @@
 const allowedOrigin = [
     "http://localhost:3000",
     "https://6vzqkvwl-5501.usw3.devtunnels.ms/",
-    "http://127.0.0.1:5502"
+    "http://127.0.0.1:5502",
+    "https://fathiabams-task.onrender.com"
 
 ];
 module.exports = allowedOrigin;
