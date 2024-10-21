161c7bdaf0e963a80ce30b83cbacf4cfedef97cb
oluwatobi ayoola jolaosho <fury25423@gmail.com>
2024-10-21 09:37:37 +0100

    fixing cors issue

--- server/config/allowedOrigin.js
+++ server/config/allowedOrigin.js
@@ -3,7 +3,7 @@ const allowedOrigin = [
     "https://6vzqkvwl-5501.usw3.devtunnels.ms/",
     "http://127.0.0.1:5502",
     "https://fathiabams-task.onrender.com",
-    "https://127.0.0.1:5502"
+    "https://127.0.0.1:5502/frontend"
 
 ];
 module.exports = allowedOrigin;
