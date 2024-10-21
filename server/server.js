514abbf7db1e5e2186afa20b69c4f5d5edc7b32b
oluwatobi ayoola jolaosho <fury25423@gmail.com>
2024-10-21 10:39:10 +0100

    fixing cors issues

--- server/server.js
+++ server/server.js
@@ -27,9 +27,9 @@ app.use(express.json());
 app.use("/api/v1", require("./router/operations"));
 
 const port = 3000;
-io.listen(1230, () => {
-  console.log("Socket server started on port 8080");
-});
+// io.listen(1230, () => {
+//   console.log("Socket server started on port 8080");
+// });
 app.listen(port, () => {
   console.log(`Server started on port ${port} `);
 });
